db8758e957341d0087bc3b4c3aed7f1c0989d817
santiago <santiago@santiago-HP-TouchSmart-tm2-Notebook-PC>
2016-05-25 18:53:53 -0300

    files

--- TPE/src/TPE/FileManager.java
+++ TPE/src/TPE/FileManager.java
@@ -1,6 +1,7 @@
 package TPE;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,8 +11,19 @@ import java.util.regex.Pattern;
 public class FileManager {
 
 	
-	public List<String> readFlights(String file){
-		return null;
+	public List<String> readFlights(String file) throws FileNotFoundException{
+		File toRead = new File("F:/git/eda-2016-04/TPE/src/Datos",file);
+        Scanner sc = new Scanner(toRead);
+        List<String> res = new LinkedList<String>();
+        while(sc.hasNextLine()){
+        	String s = sc.nextLine();
+        	String format = "[a-z A-Z]{1,3}#[0-9]{1,7}#(Lu|Ma|Mi|Ju|Vi|Sa|Do)(-(Lu|Ma|Mi|Ju|Vi|Sa|Do))*#[a-z A-Z]{3}#[a-z A-Z]{3}#([0-1][0-9]|2[0-4]):[0-5][0-9]#([1-9]h|[1-9][0-9]h])?[0-5][0-9]m$";
+	        if(!Pattern.matches(format, s)){
+	        	System.out.println("formato no valido");
+	        }
+	        res.addAll(split(s)); 	        		        	
+        }
+        return res;
 	}
 	public boolean writeRoute(List<Flight> route,String file){
 		return true;
